e6a463075470ae5716c6b5496fb0064422e0fb12
Meric Asaner <meric.a.asaner@gmail.com>
2024-04-26 13:49:56 +0300

    refactoring

--- api/index.js
+++ api/index.js
@@ -1,66 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const PORT = process.env.PORT || 4040;
-
-// const app = express();
-// app.use(express.json());
-
-// // Import the leads handler function
-// const handleLeads = require("./leads");
-
-// // Define the endpoint for handling leads
-// app.get("/leads", handleLeads);
-
-// // // Default route handling
-// // app.post("*", async (req, res) => {
-// //     console.log(req.body);
-// //     res.send("Hello Post!");
-// // });
-
-// // app.get("*", async (req, res) => {
-// //     res.send("Hello Get!");
-// // });
-
-// app.listen(PORT, function (err) {
-//     if (err) console.log(err);
-//     console.log("Server listening on http://localhost:" + PORT);
-// });
-
-// require("dotenv").config();
-
-// const express = require("express");
-// const PORT = process.env.PORT || 4040;
-
-// const app = express();
-// app.use(express.json());
-
-// // Import the leads handler function
-// const handleLeads = require("./leads");
-
-// // Import the retrieveLeadsRouter middleware
-// const retrieveLeadsRouter = require("./retrieve");
-
-// // Define the endpoint for handling leads
-// app.get("/leads", handleLeads);
-
-// // Define the endpoint for retrieving leads using the new router
-// app.use("/retrieve", retrieveLeadsRouter);
-
-// // Default route handling
-// app.post("*", async (req, res) => {
-//   console.log(req.body);
-//   res.send("Hello Post!");
-// });
-
-// app.get("*", async (req, res) => {
-//   res.send("Hello Get!");
-// });
-
-// app.listen(PORT, function (err) {
-//   if (err) console.log(err);
-//   console.log("Server listening on http://localhost:" + PORT);
-// });
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -76,6 +13,9 @@ const handleRetrieve = require("./retrieve");
 app.get("/leads", handleLeads);
 app.post("/leads", handleRetrieve);
 
+app.get("/", async (req, res) => {
+  res.send("Hello Canonical!");
+});
 // Start the server
 app.listen(PORT, function (err) {
   if (err) console.log(err);
